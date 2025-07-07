1356f85fba96efbe607cf9fd13745a09d1514d3a
reddy887 <avulareddybabu@gmail.com>
2025-07-07 20:15:27 +0530

    reddy

---
+++ reddy/baabs/pojoprogram.java
@@ -0,0 +1,12 @@
+package reddy.baabs;
+public class pojoprogram {
+	public static void main(String args[])
+	{
+		pojo pj=new pojo();
+		pj.setID(34);
+		pj.name="reddybabu";
+		pj.salary=45.0;
+		System.out.print(pj.name);
+	}
+
+}
