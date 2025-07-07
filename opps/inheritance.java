ff97de506e5cf05408f9266006ef6e7fb92aaa3c
reddy887 <avulareddybabu@gmail.com>
2025-07-07 20:14:57 +0530

    opps

---
+++ opps/inheritance.java
@@ -0,0 +1,13 @@
+package opps;
+
+public class inheritance
+{
+	public static void main(String[] args)
+	{
+		animal an=new animal();
+		an.id();
+		an.sum();
+		addition ad=new addition();
+		ad.sum();
+	}
+}
