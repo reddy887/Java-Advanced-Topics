19a41d04218bd453e2567e829f1e363dbf93960e
reddy887 <avulareddybabu@gmail.com>
2025-07-10 20:52:03 +0530

    operators added

--- operators/relational.java
+++ operators/relational.java
@@ -4,10 +4,22 @@ public class relational {
     public int check( int num)
     {
         Scanner sc =new Scanner(System.in);
-        System.out.println("enter the number or letters");
-        num =sc.nextInt();
-        
+        System.out.println("enter the number");
+        int n =sc.nextInt();
+
+        if (n==num)
+        {
+            System.out.println("the numbers are equale");
+        }
+        else if (n!=num)
+        {
+            System.out.println("the numbers are not equal");
+            if (n<num||n>num)
+            {
+                System.out.println("the number is "+n +" "+num);
+
+            }
+        }
        return num;
     }
-    
 }
