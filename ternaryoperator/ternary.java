d5125c00266d8446a0ed376b20a66a73f9f7c309
reddy887 <avulareddybabu@gmail.com>
2025-07-10 21:08:39 +0530

    ternaryoperator

---
+++ ternaryoperator/ternary.java
@@ -0,0 +1,24 @@
+package ternaryoperator;
+
+public class ternary 
+{
+	public static void main(String args[]) 
+	{
+		int n[]= {34,67,4,23,56,67};
+//		Scanner sc =new Scanner(System.in);
+		//to checking nagetive or positive
+//		System.out.println("enter the value");
+//		int n=sc.nextInt();
+//		if (n==0)
+//			System.out.println("this equal to zero");
+//		else if(n>=0)
+//			System.out.println("nagetive");
+//		else;
+//		    System.out.println("positive ");
+		//ternary operato
+		for (int i:n)
+			System.out.println(i);
+//			System.out.println((n==0)?"zero":((n<0))?"negative":"positive");
+		
+	}
+}
