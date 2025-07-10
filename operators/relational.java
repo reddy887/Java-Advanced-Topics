d448d486eded450ddb61379cc6f719af4f31a849
reddy887 <avulareddybabu@gmail.com>
2025-07-10 20:31:44 +0530

    program with relational operators

---
+++ operators/relational.java
@@ -0,0 +1,13 @@
+package operators;
+import java.util.Scanner;
+public class relational {
+    public int check( int num)
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the number or letters");
+        num =sc.nextInt();
+        
+       return num;
+    }
+    
+}
