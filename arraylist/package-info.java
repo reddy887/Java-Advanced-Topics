032153878159787b3e032ef4bf639778f9081cbb
reddy887 <avulareddybabu@gmail.com>
2025-07-11 18:57:10 +0530

    arraylist file add in the folder

--- arraylist/package-info.java
+++ arraylist/package-info.java
@@ -1,1 +1,19 @@
 package arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class arrayl
+{
+    public static void main (String args[])
+    {
+        Integer[] ar=new Integer[]{2,3,4,6,8};
+        ArrayList <Integer>arr= new java.util.ArrayList<Integer>(Arrays.asList(ar));
+        arr.add(23);
+        System.out.println(arr.contains(23));
+        for(Integer i:arr)
+        {
+            System.out.println(i);
+        }
+    }
+}
