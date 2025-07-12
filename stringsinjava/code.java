c74cabb051a355bf16813ef047507c8f13c2e52d
reddy887 <avulareddybabu@gmail.com>
2025-07-12 22:17:49 +0530

    we are checking the both heap memoryes

--- stringsinjava/code.java
+++ stringsinjava/code.java
@@ -7,6 +7,7 @@ public class code
         String st3="reddy babu";
         String st2=new String("reddy babu");//the data stored in heap memory with objects
         String st5=new String("reddy babu");
+        System.err.println(st2==st5);
         // know we are comparing the both
         System.err.println(st==st2);//output is the false
         System.err.println(st==st3);
