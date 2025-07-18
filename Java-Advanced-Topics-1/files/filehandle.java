b189bc27b26c8ec36df6497769c7a13f376070ae
reddy887 <avulareddybabu@gmail.com>
2025-07-18 22:11:28 +0530

    code modified

--- Java-Advanced-Topics-1/files/filehandle.java
+++ Java-Advanced-Topics-1/files/filehandle.java
@@ -1,10 +1,21 @@
 package files;
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 public class filehandle 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException
     {
         File f1=new File("./files theory.txt");
-        System.out.println(f1.canRead());
+        if(!f1.exists()){
+            f1.createNewFile();
+        }
+        Scanner sc=new Scanner(f1);
+        String text=" ";
+        while (sc.hasNext())
+        {
+            text=sc.nextLine();
+        }
+        System.out.println(text);
     }
 }
