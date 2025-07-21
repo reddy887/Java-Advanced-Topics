a2bc774a472059e15662ba2bba237c9870551a17
reddy887 <avulareddybabu@gmail.com>
2025-07-21 22:30:07 +0530

    linked linkedList

---
+++ linkedList/singlelinked.java
@@ -0,0 +1,18 @@
+package linkedList;
+class Node
+{
+    int data ;
+    Node next;
+    public Node(int data)
+    {
+        this.data=data;
+        this.next=null;
+    }
+}
+
+public class singlelinked
+{
+    public static void main(String[] args) {
+        
+    }
+}
